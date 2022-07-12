5aaebc7d301af2c0d15bb73c99df6f3bdb8bc7f0
AmelieLoessl <amelie.loessl@campus.lmu.de>
2022-07-12 08:56:23 +0200

    This is a new commit for what I originally planned to be amended

--- R/R-INLA.R
+++ R/R-INLA.R
@@ -6,42 +6,106 @@ library(Matrix)
 library(ggplot2)
 list.of.packages <- c("gapminder", "gt", "tidyverse")
 
-# install required packages, if necessary, and load them ----
+# install required packages, if necessary, and load them --
 {
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) install.packages(new.packages)
   lapply(list.of.packages, require, character.only = TRUE)
 }
 
-map1 <- readFITS(file = "2map-f1z1.fits")
+map1 <- readFITS(file = "data/map_rs_2.fits")
 
 map1cols <-map1$col
 temps <- map1cols[]
-unlist_temp <- unlist(temps)
+I <- unlist(temps)
 
-df_temp <- as.data.frame(unlist_temp)
+df_temp <- as.data.frame(I)
 
-CMB_temp <- as.CMBDataFrame(df_temp, nside = 512, ordering = "ring")
-CMB_temp_nested <- ordering(CMB_temp, new.ordering = "nested")
+sky <- as.CMBDataFrame(df_temp, nside = 4, ordering = "nested")
+#sky <- ordering(sky, new.ordering = "ring")
+#sky_cartesian <- CMBDataFrame(sky, coords  = "cartesian")
+#sky_spherical <- CMBDataFrame(sky, coords = "spherical")
+#CMB_temp_nested <- ordering(CMB_temp, new.ordering = "nested")
+sky[31,]
+#colnames(CMB_temp_nested) <- "I"
+#colnames(sky_cartesian$unlist_temp) <- "I"
 
-colnames(CMB_temp_nested) <- "I"
-colnames(CMB_temp) <- "I"
+#plot(CMB_temp_nested) ...
+plot(sky)
 
-plot(CMB_temp_nested)
-plot(CMB_temp)
+# plot windows slices
+window(sky_spherical) <- CMBWindow(theta = c(pi/2,pi/2,pi/2.1, pi/2.1), phi = c(0,pi/100,pi/100,0))
+plot(sky_spherical)
+
+window(sky_cartesian) <- CMBWindow(x = 0, y= 3/5, z = 4/5 ,r = 0.8, set.minus = TRUE)
+plot(sky_cartesian)
+
+## Plot Cov
+Cov <- covCMB(sky, max.dist = 3, num.bins = 10)
+plot(Cov)
+maxDist(sky)
+
+# sub_sky <- sampleCMB(sky, sample.size = 100000)
+# Cov <- covCMB(sub_sky, max.dist = 0.04, num.bins = 50)
+# plot(Cov)
+# maxDist(sub_sky)
+
+df1 <- sampleCMB(sky, sample.size = 100000)
+cov <- covCMB(df1, max.dist  = 0.03, num.bins = 20)
+cov$v
+plot(cov)
 
 
+varcmb <- variogramCMB(df1, max.dist = 0.1, num.bins = 30)
+ols <- variofitCMB(varcmb, fix.nug=FALSE, wei="equal", cov.model= "matern")
+plot(varcmb)
+lines(ols, lty=2)
+
+# visualize Neighbourhood:
+ns <- 512
+rand <- rnorm(12 * ns ^ 2)
+cmbdf <- CMBDataFrame(nside = ns, I = rand, ordering = "nested")
+w1 <- window(cmbdf, in.pixels = c(1,9))
+w2 <- window(cmbdf, in.pixels = c(2,10))
+w3 <- window(cmbdf, in.pixels = c(4,12))
+w1_sky <- window(sky, in.pixels = c(1,9))
+w2_sky <- window(sky, in.pixels = c(2,10))
+w3_sky <- window(sky, in.pixels = c(4,12))
+plot(w1, col = "blue", back.col = "white", xlab = '', ylab = '', zlab = '')
+plot(w2, col = "green", add = TRUE)
+plot(w3, col = "orange", add = TRUE)
+plot(w1_sky, col = "blue", back.col = "white", xlab = '', ylab = '', zlab = '')
+plot(w2_sky, col = "green", add = TRUE)
+plot(w3_sky, col = "orange", add = TRUE)
+displayPixelBoundaries(nside = 2,ordering = "nested",incl.labels = 1:48,col ="red")
+
+
+demoNeighbours <- function(p,j) {
+  neighbours(p, j)
+  displayPixels(boundary.j = j, j = j, plot.j = j+3,
+                spix = neighbours(p, j),
+                boundary.col = "gray",
+                boundary.lwd = 1,
+                incl.labels = neighbours(p, j),
+                col = "blue",
+                size = 3)
+  rcosmo::displayPixelBoundaries(nside = 1, col = "blue", lwd = 3)
+}
+
+demoNeighbours(1,9)
 
 # Definiere Struktur der Nachbarschaftsmatrix W für Auflösung 'res'
-res <- 1
+res <- 2
 n_side <- 2^(res)
 pixel <- 12*n_side^2
 neighbourhood <- list()
+
 for(i in seq_len(12*(n_side)^2)){
   temp_neigh <- neighbours(i,res)
   neighbourhood[[i]] <- head(temp_neigh, -1)
 }
 
+#neighbours(1,9)
 
 vector <- rep(0,12*(n_side)^2)
 temp_W <- replicate(12*(n_side)^2, vector, FALSE)
@@ -60,6 +124,38 @@ Q_inla = inla.graph2matrix(g)
 
 image(Q_inla)
 
+
+# Definiere Struktur der Nachbarschaftsmatrix W für ausgewähltes Fenster für Auflösung 'res'
+res <- 4
+n_side <- 2^(res)
+pixel <- 12*n_side^2
+neighbourhood <- list()
+
+
+for(i in seq_len(nrow(sky_spherical))){
+  temp_neigh <- neighbours(i,res)
+  neighbourhood[[i]] <- head(temp_neigh, -1)
+}
+
+vector <- rep(0,nrow(sky_spherical))
+temp_W <- replicate(nrow(sky_spherical), vector, FALSE)
+
+for(i in seq_len(nrow(sky_spherical))){
+  temp_W[[i]][neighbourhood[[i]]] <- 1
+}
+
+W <- as.matrix(as.data.table(temp_W)) # TODO: Die Präzisionsmatrix kann auch platzsparender  gespeichert werden wie in 'Bayesian Inference with INLA' beschrieben. Notwendig für höhere Auflösungen! 
+
+# Um die Struktur visualiseren zu können wird Q ausgerechnet (geht aber nicht in die Berechnung # im inla Modell ein)
+Q <- (diag((W%*%rep(1,pixel))[,1])-W) - Diagonal(pixel)
+g = inla.read.graph(Q)
+Q_inla = inla.graph2matrix(g)
+
+
+image(Q_inla)
+
+
+
 # Umordnung zur Bandmatrix
 # g.file = inla.graph2matrix(g)
 # inla.spy(g.file,  reordering = inla.qreordering(g,reordering = "band"))
@@ -68,20 +164,20 @@ image(Q_inla)
 # Folgende Simulation dient als Dummy, da Einlesen der Fits Datei noch nicht funktioniert!
 # Simulation erfogt für Auflösung 1. Nachbarschaftsstruktur wird nicht! simuliert, ist also nur # eine sehr grobe Simulation!
 
-nLags = 48 # number of lags (size of region)
-# fake, uncorrelated observations
-set.seed(1)
-X = rlnorm(nLags)/10000
-###############################################
-# fake sigma... correlated decreases distance.
-sigma = diag(nLags)
-corr = 0.05
-sigma <- corr ^ abs(row(sigma)-col(sigma))
-###############################################
-# Y is autocorrelated...
-sky_sim <- t(X %*% chol(sigma))
-set.seed(1)
-sky_sim <- sky_sim + abs(rnorm(48,0,2)/10000)
+# nLags = 48 # number of lags (size of region)
+# # fake, uncorrelated observations
+# set.seed(1)
+# X = rlnorm(nLags)/10000
+# ###############################################
+# # fake sigma... correlated decreases distance.
+# sigma = diag(nLags)
+# corr = 0.05
+# sigma <- corr ^ abs(row(sigma)-col(sigma))
+# ###############################################
+# # Y is autocorrelated...
+# sky_sim <- t(X %*% chol(sigma))
+# set.seed(1)
+# sky_sim <- sky_sim + abs(rnorm(48,0,2)/10000)
 
 
 
@@ -214,9 +310,10 @@ quit <- function() {
 CAR.model <- inla.rgeneric.define(inla.rgeneric.CAR.model, W = W)
 
 # add index for latent effect
-dt_sky <-as.data.table(cbind(seq_len(nrow(sky_sim)), sky_sim))
+dt_sky <-as.data.table(cbind(seq_len(nrow(sky)), sky))
 colnames(dt_sky) <- c("id_x", "obs")
 df_sky <- as.data.frame(dt_sky)
+df_sky$obs <- df_sky$obs + 1 # wie soll mit den negativen Werten umgegangen werden?
 
 # set car formular 
 f.car <- obs ~ 1 + f(id_x, model = CAR.model)
@@ -224,6 +321,7 @@ f.car <- obs ~ 1 + f(id_x, model = CAR.model)
 # run inla
 m.car <- inla(f.car, data = df_sky, family = "lognormal")
 
+ggplot(data = df_sky, aes(x = id_x, y = obs)) + geom_line()
 
 summary(m.car)
 
@@ -296,12 +394,11 @@ c_format = "png"
 
 # get table with metrics
 metrics_table %>% gt()
-```
-\
 
-**Darstellung der posteriori marginals der unterschiedlichen Strategien in Modell Repräsentation**
+
+#Darstellung der posteriori marginals der unterschiedlichen Strategien in Modell Repräsentation
   
-  ```{r, echo = FALSE}
+
 num <- 0
 post_marg.rho_list <- list()
 post_marg.prec_list <- list()
@@ -312,9 +409,13 @@ op <- c("ccd", "grid", "eb")
 for(i in seq_len(length(c("gaussian", "simplified.laplace", "laplace")))){
   for(j in seq_len(length(c("ccd", "grid", "eb")))){
     num <- num + 1
-    marg.prec_temp <- inla.tmarginal(exp, m.strategy[[i]][[j]]$marginals.hyperpar[[1]])
-    marg.rho_temp <- inla.tmarginal(function(x) { 1/(1 + exp(-x))},
-                                    m.strategy[[i]][[j]]$marginals.hyperpar[[2]])
+    # marg.prec_temp <- inla.tmarginal(exp, m.strategy[[i]][[j]]$marginals.hyperpar[[1]])
+    # marg.rho_temp <- inla.tmarginal(function(x) { 1/(1 + exp(-x))},
+    #                                 m.strategy[[i]][[j]]$marginals.hyperpar[[2]])
+    
+    
+    marg.prec_temp <- m.strategy[[i]][[j]]$marginals.hyperpar[[1]]
+    marg.rho_temp <- m.strategy[[i]][[j]]$marginals.hyperpar[[2]]
     
     df_marg.rho_temp <- as.data.frame(marg.rho_temp)
     df_marg.prec_temp <- as.data.frame(marg.prec_temp)
